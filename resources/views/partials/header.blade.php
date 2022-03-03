ab36771796629c77ffd6d4b3c10d478805ec377a
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-03-03 15:33:40 +0100

    header nav bar

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+<div class="header-container">
+    <div class="logo">
+        <img src="img/dc-logo.png" alt="">
+    </div>
+    <ul>
+        <li>
+            <a href="{{url('/')}}">characters</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">comics</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">movies</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">tv</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">games</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">videos</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">fans</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">news</a>
+        </li>
+        <li>
+            <a href="{{url('/')}}">shop</a>
+        </li>
+        <li>
+            <input type="text" placeholder="Search">
+            <button><i class="fas fa-search"></i></button>
+        </li>
+    </ul>
+</div>
